dc5f5c862542d434465db025f8809c237116dc91
Gustavo Linhaus <gustavo_linhaus@hotmail.com>
2022-07-20 20:20:02 -0300

    Category resource

---
+++ backend/src/main/java/com/catalog/entities/Category.java
@@ -0,0 +1,16 @@
+package com.catalog.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category implements Serializable {
+    private Long id;
+    private String name;
+}
