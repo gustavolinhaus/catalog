55b6b86b819559134a07fc9c44abeea72ed85d92
Gustavo Linhaus <gustavo_linhaus@hotmail.com>
2022-07-22 16:24:41 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/catalog/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import com.catalog.services.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,11 @@ public class CategoryResource {
     @ResponseStatus(HttpStatus.OK)
     public List<CategoryDTO> findAll() {
         return categoryService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO findById(@PathVariable Long id) {
+        return categoryService.findById(id);
     }
 }
--- backend/src/main/java/com/catalog/services/CategoryService.java
+++ backend/src/main/java/com/catalog/services/CategoryService.java
@@ -3,11 +3,13 @@ package com.catalog.services;
 import com.catalog.dto.CategoryDTO;
 import com.catalog.entities.Category;
 import com.catalog.repositories.CategoryRepository;
+import com.catalog.services.exceptions.EntityNotFoundException;
 import com.catalog.services.mapper.CategoryMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +22,12 @@ public class CategoryService {
     public List<CategoryDTO> findAll() {
         List<Category> categories = categoryRepository.findAll();
         return categoryMapper.toDtos(categories);
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return categoryMapper.toDto(category);
     }
 
 }
