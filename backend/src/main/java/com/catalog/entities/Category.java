ca0c6d6051bff492a9a85e9bfd52b9f05c7f5af6
Gustavo Linhaus <gustavo_linhaus@hotmail.com>
2022-07-25 19:16:39 -0300

    Product entity, SQL script, remove @NoArgsConstructor and @AllArgsConstructor

--- backend/src/main/java/com/catalog/entities/Category.java
+++ backend/src/main/java/com/catalog/entities/Category.java
@@ -1,8 +1,6 @@
 package com.catalog.entities;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,8 +13,6 @@ import java.io.Serializable;
 @Table(name = "tb_category")
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Category implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ backend/src/main/java/com/catalog/entities/Product.java
@@ -0,0 +1,44 @@
+package com.catalog.entities;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_product")
+@Getter
+@Setter
+public class Product implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant date;
+
+    @Setter(AccessLevel.NONE)
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    Set<Category> categories = new HashSet<>();
+}
