eb4a8307ad0e679bf4815dc9cf2b3000b8c3605e
Gustavo Linhaus Fernandes <gustavo.fernandes@basis.com.br>
2022-07-26 13:05:49 -0300

    Impl interface category and product

--- backend/src/main/java/com/catalog/services/CategoryService.java
+++ backend/src/main/java/com/catalog/services/CategoryService.java
@@ -1,60 +1,13 @@
 package com.catalog.services;
 
 import com.catalog.dto.CategoryDTO;
-import com.catalog.entities.Category;
-import com.catalog.repositories.CategoryRepository;
-import com.catalog.services.exceptions.DatabaseException;
-import com.catalog.services.exceptions.ResourceNotFoundException;
-import com.catalog.services.mapper.CategoryMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@RequiredArgsConstructor
-public class CategoryService {
-
-    private final CategoryRepository categoryRepository;
-    private final CategoryMapper categoryMapper;
-
-    @Transactional(readOnly = true)
-    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
-        return categoryRepository.findAll(pageRequest).map(categoryMapper::toDTO);
-    }
-
-    @Transactional(readOnly = true)
-    public CategoryDTO findById(Long id) {
-        Category category = categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
-        return categoryMapper.toDTO(category);
-    }
-
-    @Transactional
-    public CategoryDTO insert(CategoryDTO categoryDTO) {
-        Category category = categoryMapper.toEntity(categoryDTO);
-        category = categoryRepository.save(category);
-        return categoryMapper.toDTO(category);
-    }
-
-    @Transactional
-    public CategoryDTO update(Long id, CategoryDTO categoryDTO) {
-        Category category = categoryMapper.toEntity(categoryDTO);
-        category.setId(categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Entity not found")).getId());
-        category = categoryRepository.save(category);
-        return categoryMapper.toDTO(category);
-    }
-
-    @Transactional
-    public void delete(Long id) {
-        try {
-            categoryRepository.deleteById(id);
-        } catch (EmptyResultDataAccessException exception) {
-            throw new ResourceNotFoundException("Entity not found");
-        } catch (DataIntegrityViolationException exception) {
-            throw new DatabaseException("Integrity violation");
-        }
-    }
+public interface CategoryService {
+    Page<CategoryDTO> findAllPaged(PageRequest pageRequest);
+    CategoryDTO findById(Long id);
+    CategoryDTO insert(CategoryDTO categoryDTO);
+    CategoryDTO update(Long id, CategoryDTO categoryDTO);
+    void delete(Long id);
 }
---
+++ backend/src/main/java/com/catalog/services/impl/CategoryServiceImpl.java
@@ -0,0 +1,62 @@
+package com.catalog.services.impl;
+
+import com.catalog.dto.CategoryDTO;
+import com.catalog.entities.Category;
+import com.catalog.repositories.CategoryRepository;
+import com.catalog.services.CategoryService;
+import com.catalog.services.exceptions.DatabaseException;
+import com.catalog.services.exceptions.ResourceNotFoundException;
+import com.catalog.services.mapper.CategoryMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    @Transactional(readOnly = true)
+    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+        return categoryRepository.findAll(pageRequest).map(categoryMapper::toDTO);
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return categoryMapper.toDTO(category);
+    }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO categoryDTO) {
+        Category category = categoryMapper.toEntity(categoryDTO);
+        category = categoryRepository.save(category);
+        return categoryMapper.toDTO(category);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO categoryDTO) {
+        Category category = categoryMapper.toEntity(categoryDTO);
+        category.setId(categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Entity not found")).getId());
+        category = categoryRepository.save(category);
+        return categoryMapper.toDTO(category);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        try {
+            categoryRepository.deleteById(id);
+            categoryRepository.flush();
+        } catch (EmptyResultDataAccessException exception) {
+            throw new ResourceNotFoundException("Entity not found");
+        } catch (DataIntegrityViolationException exception) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
+}
